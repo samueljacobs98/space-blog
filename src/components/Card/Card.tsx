7fd28f6f02c219a87eedc737404024c141935b75
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-04-29 17:05:45 +0100

    updated date format

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -16,13 +16,20 @@ const Card: React.FC<CardProps> = ({
   newsSite,
   publishedAt,
 }) => {
+  const updateDate = (unformattedDate: string): string => {
+    const dateArr = unformattedDate.split("-");
+    return `${dateArr[2]}/${dateArr[1]}/${dateArr[0]}`;
+  };
+
   return (
     <div className="card">
       <img className="card__image" src={imageUrl} alt={title} />
       <div className="card__info">
         <p className="card__info-source">{newsSite}</p>
         <h2 className="card__info-title">{title}</h2>
-        <p className="card__info-date">{publishedAt.slice(0, 10)}</p>
+        <p className="card__info-date">
+          {updateDate(publishedAt.slice(0, 10))}
+        </p>
       </div>
       <a href={url} className="card__rocket" target="_blank" rel="noreferrer">
         <img src={rocket} alt="" />
